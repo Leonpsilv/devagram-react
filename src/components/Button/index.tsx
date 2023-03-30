1df9a97d71532f467c6241d01a0fc2f73ec797b8
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-03-29 22:42:34 -0400

    button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,26 @@
+const Button = ({
+    type = 'button',
+    text,
+    color = 'primary',
+    disabled = false,
+    onClick
+}: {
+    type: any
+    text: string
+    color: string
+    disabled: boolean
+    onClick: any
+}) => {
+    return (
+        <button
+            type={type}
+            className={`btn ${color}`}
+            disabled={disabled}
+            onClick={onClick}
+        >
+            {text}
+        </button>
+    )
+}
+
+export default Button
