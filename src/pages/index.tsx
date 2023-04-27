85904325a029f3fe580bde506f1e5eff836711f3
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-04-26 21:44:49 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,12 @@ import Home from "@/components/home";
 
 const userService = new UserService()
 export default function Index() {
-  const [authenticate, setAuthenticate] = useState(false)
+  const [authenticate, setAuthenticate] = useState<any>(null)
   useEffect(()=> {
     setAuthenticate(userService.isAuthenticate())
   }, [])
+
+  if (authenticate === null) return null;
 
   if(authenticate) return <Home />
   
